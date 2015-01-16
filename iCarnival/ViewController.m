cfa306c235a049482f9d73df5adc77c93b6b29bb
Nathan Wallace <techietotoro@gmail.com>
2015-01-15 20:06:33 -0800

    Initial Commit

---
+++ iCarnival/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iCarnival
+//
+//  Created by Nathan Wallace on 1/15/15.
+//  Copyright (c) 2015 Punahou School - Nathan Wallace. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iCarnivalTests/iCarnivalTests.m
@@ -0,0 +1,40 @@
+//
+//  iCarnivalTests.m
+//  iCarnivalTests
+//
+//  Created by Nathan Wallace on 1/15/15.
+//  Copyright (c) 2015 Punahou School - Nathan Wallace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iCarnivalTests : XCTestCase
+
+@end
+
+@implementation iCarnivalTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
