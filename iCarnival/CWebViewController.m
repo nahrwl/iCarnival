c258a82a6cc1e433a563fef83311c3d4d2a306c1
Nathan Wallace <techietotoro@gmail.com>
2016-01-16 12:20:54 -0800

    Revamp how social tab wkwebview works

--- iCarnival/CWebViewController.m
+++ iCarnival/CWebViewController.m
@@ -12,9 +12,12 @@
 @interface CWebViewController () <WKNavigationDelegate, WKUIDelegate, WKScriptMessageHandler>
 
 @property (strong, nonatomic) WKWebView *wv;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 
 @property (nonatomic) CGPoint featuredOffset;
 @property (nonatomic) CGPoint latestOffset;
+
+@property (strong, nonatomic) WKNavigation *currentNavigation;
 
 @end
 
@@ -26,11 +29,28 @@
     self.segmentedControl.hidden = YES;
     
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    config.suppressesIncrementalRendering = YES;
     //config.applicationNameForUserAgent = @"iCarnival Punahou Carnival";
+    
+    // Add CSS styles to the config
+    NSString *bodyScriptContent = @"var styleTag = document.createElement(\"style\"); styleTag.textContent = 'body {display:none;}'; document.documentElement.appendChild(styleTag);";
+    WKUserScript *bodyScript = [[WKUserScript alloc] initWithSource:bodyScriptContent injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES];
+    [config.userContentController addUserScript:bodyScript];
+    
+    NSString *scriptContent = @"var styleTag = document.createElement(\"style\"); styleTag.textContent = 'header, footer, #global-navbar {display:none;} .container-fluid.tb-wrapper {margin-top: 20px;}'; document.documentElement.appendChild(styleTag);";
+    WKUserScript *script = [[WKUserScript alloc] initWithSource:scriptContent injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
+    [config.userContentController addUserScript:script];
+    
     self.wv = [[WKWebView alloc] initWithFrame:CGRectZero configuration:config];
     self.wv.navigationDelegate = self;
     WKUserContentController *userContentController = self.wv.configuration.userContentController;
     [userContentController addScriptMessageHandler:self name:@"notification"];
+    
+    // Add refresh control to web view
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    [refreshControl addTarget:self action:@selector(refreshControlValueChanged:) forControlEvents:UIControlEventValueChanged];
+    self.refreshControl = refreshControl;
+    // The refresh control is not added as a subview until the first page load is complete
     
     self.wv.hidden = YES;
     
@@ -47,11 +67,29 @@
     self.wv.scrollView.minimumZoomScale = 1.0;
     
     self.featuredOffset = CGPointMake(0.0, -64.0);
-    self.latestOffset = CGPointMake(0.0, -64.0);;
+    self.latestOffset = CGPointMake(0.0, -64.0);
     
-    [self.wv loadRequest:[NSURLRequest
+    self.currentNavigation = [self.wv loadRequest:[NSURLRequest
                           requestWithURL:[NSURL URLWithString:@"https://tagboard.com/punahoucarnival/208630"]]];
     
+}
+
+- (void)refreshControlValueChanged:(UIRefreshControl *)refreshControl {
+    //refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"Updating #PunahouCarnival"];
+    
+    self.currentNavigation = [self.wv reload];
+    
+    // Reset relevant variables
+    self.featuredOffset = CGPointMake(0.0, -64.0);
+    self.latestOffset = CGPointMake(0.0, -64.0);
+    
+    // Quickly hide the web view
+    [UIView animateWithDuration:0.5 animations:^{
+        //self.wv.layer.opacity = 0;
+    } completion:^(BOOL finished) {
+        // Nothing right now
+        //self.wv.hidden = YES;
+    }];
 }
 
 - (IBAction)segmentedControlChanged:(UISegmentedControl *)sender {
@@ -88,19 +126,25 @@
 }
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
-    static BOOL firstLoad = YES;
-    if (firstLoad) {
-        [self.wv evaluateJavaScript: @"jQuery('header').hide(); jQuery('footer').hide(); jQuery('#global-navbar').hide(); jQuery('.container-fluid.tb-wrapper').css('margin-top','20px'); $('#posts').on('tgb:featuredLoaded', function() { window.webkit.messageHandlers.notification.postMessage('complete'); jQuery('.owned').unbind('click'); });"
+    if ([self.currentNavigation isEqual:navigation]) {
+        // End refreshing (if necessary)
+        [self.refreshControl endRefreshing];
+        
+        // Clean up the page
+        /*[self.wv evaluateJavaScript: @"jQuery('header').hide(); jQuery('footer').hide(); jQuery('#global-navbar').hide(); jQuery('.container-fluid.tb-wrapper').css('margin-top','20px'); $('#posts').on('tgb:featuredLoaded', function() { window.webkit.messageHandlers.notification.postMessage('complete'); jQuery('.owned').unbind('click'); });"
+                  completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+                      NSLog(@"onLoad JS complete.");
+                  }];*/
+        [self.wv evaluateJavaScript: @"jQuery('body').show(); $('#posts').on('tgb:featuredLoaded', function() { window.webkit.messageHandlers.notification.postMessage('complete'); jQuery('.owned').unbind('click'); });"
                   completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
                       NSLog(@"onLoad JS complete.");
                   }];
-        firstLoad = NO;
     }
 }
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
     static BOOL firstLoad = YES;
-    if (firstLoad) {
+    if (firstLoad || navigationAction.navigationType == WKNavigationTypeReload) {
         decisionHandler(WKNavigationActionPolicyAllow);
         firstLoad = NO;
     } else {
@@ -135,6 +179,8 @@
             self.wv.layer.opacity = 1.0;
         } completion:^(BOOL finished) {
             self.segmentedControl.userInteractionEnabled = YES;
+            // Add in the refresh control
+            [self.wv.scrollView addSubview:self.refreshControl];
         }];
     }
 }
@@ -174,7 +220,7 @@
 
 -(BOOL) webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType {
     if ( inType == UIWebViewNavigationTypeLinkClicked ) {
-        [[UIApplication sharedApplication] openURL:[inRequest URL]];
+        //[[UIApplication sharedApplication] openURL:[inRequest URL]];
         return NO;
     }
     
